3fdd36a2eda9968ded7ae7df2e358dbbfb72599d
aidan.mattson@smoothstack.com <aidan.mattson@wsu.edu>
2021-11-30 14:22:29 -0800

    Removed auth dependencies from Users

--- src/main/java/com/ss/training/utopia/security/AuthenticationFilter.java
+++ src/main/java/com/ss/training/utopia/security/AuthenticationFilter.java
@@ -2,87 +2,94 @@ package com.ss.training.utopia.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ss.training.utopia.dto.LoginDto;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-    private final AuthenticationManager authenticationManager;
+@Component
+public class AuthenticationFilter extends BasicAuthenticationFilter {
 
     public AuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
+        super(authenticationManager);
     }
 
     /**
-     * Triggers on any POST to /login
-     * Grabs JSON information for a login object and confirms user authentication
-     * @param request request information including headers
-     * @param response response template for building
-     * @return authentication token if it is created
+     * Endpoint hit by every request
+     * @param request Http request with header information
+     * @param response response template to begin building response
+     * @param chain the filter chain we are using
      */
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        // get credentials as DTO from HTTP request
-        LoginDto credentials;
-        try {
-            credentials = new ObjectMapper().readValue(request.getInputStream(), LoginDto.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
+        // grab header
+        String header = request.getHeader(SecurityConstants.HEADER_STRING);
+
+        // check header exists and is correctly formatted
+        if (header == null || !header.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
         }
 
-        // create token from DTO
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-            credentials.getUsername(),
-            credentials.getPassword(),
-            new ArrayList<>());
+        // get verified
+        Authentication authentication = getAuthenticationToken(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        // pass token to authentication
-        return authenticationManager.authenticate(authenticationToken);
+        // continue filter
+        chain.doFilter(request, response);
     }
 
     /**
-     * Runs automatically on successful authentication
-     * Creates the JWT token to use and attaches it as a header to the response
-     * @param request request information, including headers
-     * @param response response template to add information to
-     * @param chain filter chain to use for custom and built-in filters
-     * @param authResult authorization result object from the previous method
+     * Parses the Http request to determine user authorization
+     * @param request Http request to parse Jwt Tokens from
+     * @return the authorizations (if found), or null otherwise
      */
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) {
-        // get User from authorization result
-        UserDetailsImpl user = (UserDetailsImpl) authResult.getPrincipal();
-        String auth = user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+    private UsernamePasswordAuthenticationToken getAuthenticationToken(HttpServletRequest request) {
 
-        // create token for user
-        String token = JWT.create()
-            .withSubject(user.getUsername())
-            .withIssuer(SecurityConstants.ISSUER)
-            .withClaim(SecurityConstants.USER_ID_CLAIM, user.getId())
-            .withClaim(SecurityConstants.AUTHORITY_CLAIM, auth)
-            .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
-            .sign(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()));
+        // grab token
+        String token = request.getHeader(SecurityConstants.HEADER_STRING);
 
-        // add JWT token to response header
-        response.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + " " + token);
+        if (token != null) {
+            // get JWT token info
+            DecodedJWT jwt = JWT.require(Algorithm.HMAC512(SecurityConstants.SECRET))
+                .build()
+                .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""));
+
+            // check real user
+            String username = jwt.getSubject();
+            if (username == null) return null;
+
+            // get authorities
+            String claims = jwt.getClaim(SecurityConstants.AUTHORITY_CLAIM)
+                .asString();
+            List<SimpleGrantedAuthority> authorities =
+                new ArrayList<>(Arrays.asList(claims.split(",")))
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+
+            // get uid
+            Integer userId = jwt.getClaim(SecurityConstants.USER_ID_CLAIM).asInt();
+
+            // compose
+            return new UsernamePasswordAuthenticationToken(userId, null, authorities);
+        }
+        return null;
     }
 }
